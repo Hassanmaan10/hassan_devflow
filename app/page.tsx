34b3a67659d42a8301522251795fdd3a30aee83e
Hassan <hassanmaan705@gmail.com>
2024-11-18 12:31:45 +0200

    Tailwind CSS Setup & Theming

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,3 @@
-const Home = () => (
-  <h1 className="text-3xl font-black text-white">
-    Welcome to the world of next.js
-  </h1>
-);
+const Home = () => <h1 className="h1-bold">Welcome to the world of next.js</h1>;
 
 export default Home;
