5d4571c31e7792ad7b738e1f5486d5ed7297f63a
Hassan <hassanmaan705@gmail.com>
2025-01-25 16:48:59 +0200

    Assets and Metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 const Home = () => (
-  <h1 className="text-3xl text-primary-500 font-black">
-    welcome to the world of next.js
-  </h1>
+  <>
+    <h1 className="h1-bold">welcome to the world of next.js</h1>
+    <h1 className="h1-bold font-space-grotesk">
+      welcome to the world of next.js
+    </h1>
+  </>
 );
 
 export default Home;
