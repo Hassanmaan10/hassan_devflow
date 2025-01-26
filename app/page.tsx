5dae6d20d5b9105508d18e38ea8bf04d1bce1d37
Hassan <hassanmaan705@gmail.com>
2025-01-26 17:13:01 +0200

    Light & Dark Theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
 const Home = () => (
   <>
     <h1 className="h1-bold">welcome to the world of next.js</h1>
-    <h1 className="h1-bold font-space-grotesk">
-      welcome to the world of next.js
-    </h1>
   </>
 );
 
